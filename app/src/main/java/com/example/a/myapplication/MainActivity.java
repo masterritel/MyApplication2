f54543a394819c5d3e48b8651289d8e771bfe300
a <salhamanai@gmailcom>
2018-03-15 12:52:25 +0100

    test

---
+++ app/src/main/java/com/example/a/myapplication/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.a.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+        handleIntent(getIntent());
+    }
+
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        String appLinkAction = intent.getAction();
+        Uri appLinkData = intent.getData();
+        if (Intent.ACTION_VIEW.equals(appLinkAction) && appLinkData != null) {
+            String recipeId = appLinkData.getLastPathSegment();
+            Uri appData = Uri.parse("content:://com.recipe_app/recipe/").buildUpon()
+                    .appendPath(recipeId).build();
+            showRecipe(appData);
+
+
+        }
+    }
+
+    private void showRecipe(Uri appData) {
+    }
+}
